aaec62bf1a740fdcd83d28abd82d96a1f8399e03
KDA <dk091991@gmail.com>
2024-04-25 01:55:56 +0300

    Create profile form and update notifications.html

--- usersapp/views.py
+++ usersapp/views.py
@@ -1,8 +1,9 @@
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import UserLoginForm, UserSignupForm
+from .forms import UserLoginForm, UserSignupForm, ProfileForm
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
@@ -15,7 +16,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                
+                messages.success(request, f'{username} Вы вошли в аккаунт.')
                 return HttpResponseRedirect(reverse('mainapp:index'))
     else:
         form = UserLoginForm()
@@ -34,6 +35,7 @@ def signup(request):
         form = UserSignupForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, f'{request.user.username} Поздравляем! Вы успешно зарегистрировались.')
             return HttpResponseRedirect(reverse('usersapp:login'))
     else:
         form = UserSignupForm()
@@ -44,14 +46,25 @@ def signup(request):
     }
     return render(request, 'usersapp/signup.html', context)
 
-
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профиль успешно обновлен.')
+            return HttpResponseRedirect(reverse('usersapp:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
-        'title': 'Личный Кабинет'
+        'title': 'Личный Кабинет',
+        'form': form
     }
     return render(request, 'usersapp/profile.html', context)
 
-
+@login_required
 def logout(request):
+    messages.success(request, f'{request.user.username} Вы вышли из аккаунта.')
     auth.logout(request)
     return redirect(reverse('mainapp:index'))
